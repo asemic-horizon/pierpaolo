832aecabfc263b586cb4ba323eb4d10aed233c26
Diego Navarro <the.electric.me@gmail.com>
2019-05-21 19:28:30 +0000

    refactor for motion to be in persona/thing

--- pierpaolo.py
+++ pierpaolo.py
@@ -3,11 +3,13 @@ import sys
 from itertools import product, combinations
 from dataclasses import dataclass
 from enum import Enum
+from typing import List
 from random import randint
 # grammar
 with open("pierpaolo.lark") as handle: grammar = ''.join(handle.readlines())
 parser = Lark(grammar)
 
+def sub(subtree,i=0): return subtree.children[i].value
 def make_ascii_line(marker_values, max_width = 80, max_abs = 10):
     factor = max_width/(2*max_abs)
     line = max_width*"-"
@@ -25,70 +27,51 @@ class Plan(str,Enum):
     left = "LEFF"
     right = "RIGHT"
     random = "RANDOM"
-@dataclass
-class Persona:
-    name: str
-    plan : Plan
-    xpos: float
+    fixed = "FIXED"
 
+@dataclass
+class Thing:
+    name: str
+    plan : Plan = "RANDOM"
+    xpos = 0
+    history = [0]
+
+    def move(self):
+        if self.plan == "RIGHT": self.xpos +=1
+        elif self.plan == "LEFT": self.xpos -=1
+        elif self.plan == "RANDOM": self.xpos += randint(-1,1)
+        self.history.append(self.xpos)
+ 
+    def set_plan(self, plan):
+        self.plan = plan
 
 class Opera:
     def __init__(self):
-        self.characters = dict()
-        self.history = dict() # dict of lists
+        self.cast = dict()
         self.time = 0
-    def _set_persona(self,persona):
-        self.characters[persona.name] = persona
-        self.history[(persona.name,self.time)] = persona 
-    def _check_name(self, name):
-        if name not in self.characters:
-            raise ValueError(f'{name} NOT INTRODUCED')
-        else:
-            return True
-    def _update_persona(self,persona):
-        if self._check_name(persona.name):
-            self._set_persona(Persona(
-                        name = persona.name, 
-                        xpos = persona.xpos, 
-                        plan = persona.plan))
+
     def _pos_dict(self):
-        return {persona: self.characters[persona].xpos for persona in self.characters}
+        return {persona: self.cast[persona].xpos for persona in self.cast}
     def _check_collisions(self):
-        return all([self.characters[a].xpos!=self.characters[b].xpos\
-                    for a,b in combinations(self.characters.keys(),2)])
-    def execute_introduce(self,name):
-        self._set_persona(Persona(
-                        name = name, 
-                        xpos = 0, 
-                        plan = 'RANDOM'))
-    def prepare_move(self,name, plan):
-        persona = self.characters[name]
-        self.characters[name] = Persona(
-                        name = name, 
-                        xpos = persona.xpos, 
-                        plan = plan)
-    def execute_move(self,persona):
-        plan = persona.plan; xpos = persona.xpos
-        if plan == "RIGHT": xpos +=1
-        elif plan == "LEFT": xpos -=1
-        elif plan == "RANDOM": xpos += randint(-1,1)
-        self._update_persona(Persona(
-                        name =persona.name, 
-                        xpos = xpos,
-                        plan = "RANDOM"))
+        return all([self.cast[a].xpos!=self.cast[b].xpos\
+                    for a,b in combinations(self.cast.keys(),2)])
+
     def vale(self):
         self.time +=1
-        for name in self.characters:
-            self.execute_move(self.characters[name]) 
+        for persona in self.cast.values(): persona.move()
+
         if not self._check_collisions():
             print(16*'CRASH')
         print(f'{self.time:03d}'+make_ascii_line(self._pos_dict()))
 
     def run_act(self,subtree):
         for token in subtree.find_data('introduction'):
-            self.execute_introduce(token.children[0].value)
+            name = sub(token)
+            assert name not in self.cast, f"{name} PREVIOUSLY INTRODUCED ALREADY"
+            self.cast[name] = Thing(name = name, plan="RANDOM")
+
         for token in subtree.find_data('motion'):
-            self.prepare_move(name=token.children[0].value, plan=token.children[1].value)
+            self.cast[sub(token)].set_plan(sub(token,1))
         self.vale()
 
 opera = Opera()
@@ -96,4 +79,4 @@ print(83*"=")
 for num, verse in enumerate(tree.find_data('step')):
     #print(verse)
     opera.run_act(verse)
-    #print(f"-------------\nACT{num+1}: DRAMATIS PERSONAE", '\n', opera.characters)
+    #print(f"-------------\nACT{num+1}: DRAMATIS PERSONAE", '\n', opera.cast)
